8276ffea0d9f8824a7fb9b877f3fb01d4d681985
Antoine COULON <a.antoinecoulon@gmail.com>
2025-05-02 13:14:29 +0200

    Finish S2013 - close #35

--- src/main/java/fr/eni/ludotheque/bll/GameServiceImpl.java
+++ src/main/java/fr/eni/ludotheque/bll/GameServiceImpl.java
@@ -5,14 +5,24 @@ import fr.eni.ludotheque.dal.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class GameServiceImpl implements GameService{
 
-    @Autowired
-    private GameRepository gameRepository;
+    private final GameRepository gameRepository;
+
+    public GameServiceImpl(GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
 
     @Override
     public Game addGame(Game game) {
         return gameRepository.save(game);
+    }
+
+    @Override
+    public List<Game> getAllGames() {
+        return gameRepository.findAll();
     }
 }
